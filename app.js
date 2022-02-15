7cc18ad9a14d0c4d0b689f735fbd67a0062b3d16
VanessaLin9 <vanessa7591@gmail.com>
2022-02-16 06:57:46 +0800

    add handlebars

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express()
 
 const PORT = 3000
 
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req,res)=> {
-  res.send('Hello!')
+  res.render('index')
 })
+
 
 app.listen(PORT, ()=> {
   console.log(`Express is running on http://localhost${PORT}`)
