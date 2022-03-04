f26f18b70dcd24dbc003ab4a108a5a607915cf1a
VanessaLin9 <vanessa7591@gmail.com>
2022-03-04 14:00:59 +0800

    style view

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 const PORT = 3000
 const bcrypt = require('bcryptjs')
 
 const exphbs = require('express-handlebars')
 
+app.use(express.static('public'))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true })) //使用express內建的body-parser
+app.use(methodOverride('_method'))
 
 app.get('/', (req,res)=> {
   res.render('index')
