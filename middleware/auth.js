f868b171c4e7927c6cc42780a82c91d7762ff597
VanessaLin9 <vanessa7591@gmail.com>
2022-03-11 15:18:49 +0800

    add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+const { ensureAuthenticated } = require('../helpers/auth-helpers')
+const authenticated = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+
+module.exports = {
+  authenticated
+}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const passport = require('../config/passport')
 
 const recordController = require('.././controllers/record_controller')
 const userController = require('../controllers/user_controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.get('/signup', userController.signUpPage)
@@ -13,7 +14,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/host', recordController.getHomePage)
+router.get('/host', authenticated, recordController.getHomePage)
 router.get('/', (req, res) => res.redirect('/host'))
 router.use('/', generalErrorHandler)
 
