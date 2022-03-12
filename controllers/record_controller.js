c035221c98de260a965f03ef7817fcff29ec33a7
VanessaLin9 <vanessa7591@gmail.com>
2022-03-12 13:58:25 +0800

    style index page

--- controllers/record_controller.js
+++ controllers/record_controller.js
@@ -1,6 +1,16 @@
+const {Player} = require('../models')
 const recordController = {
-  getHomePage: (req, res) => {
-    return res.render('index')
+  getHomePage: (req, res, next) => {
+    Player.findAll({
+      raw:true
+    })
+    .then(players => 
+      // {players = players.map(d => ({
+      //   ...d.dataValues
+      //   Id: d.Id.dataValues,
+      // }))
+       res.render('index', {players})
+    )
   },
   addRecord: (req, res) => {
     return res.render('index')
