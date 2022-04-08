479ee2e6325168933ef842cdbce2afcaf134528a
VanessaLin9 <vanessa7591@gmail.com>
2022-04-08 00:16:32 -0400

    fix date value in index page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,10 +2,11 @@ const moment = require('moment')
 
 module.exports = {
   today: function () {
-    let a = new Date()
-    return moment(a).format('YYYY-MM-DD')
+    let a = moment().format('YYYY-MM-DD')
+    return a
+    
   },
   date: function(a) {
-    return moment(a).locale('zh-tw').format('L')
+    return moment(a).format('L')
   }
 }
